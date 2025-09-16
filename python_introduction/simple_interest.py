bcb6116efda6f55639dc1a7807942de6614e82ed
Khomotso <pascal.makananise@gmail.com>
2025-09-16 20:11:35 +0200

    Add Python exercises for basic_operations and simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# Simple Interest Calculator
+
+# Variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculation
+interest = principal * rate * time
+
+# Output
+print("The simple interest is:", interest)
+
